871b76da37163a9187e0ef573511ba10fcfc53ce
stic-server-open <1138705738@qq.com>
2021-05-29 10:38:11 +0800

    msm8996-common: The default time zone is Shanghai (China Standard Time)
    * Change the default time to China Shanghai time, save setting
    
    Change-Id: If121f2539647083bec6904bf89b697dafe21100a

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -178,3 +178,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0
+
+# Paper System Other
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.timezone=Asia/Shanghai
